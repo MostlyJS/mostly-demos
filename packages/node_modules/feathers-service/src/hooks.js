be16fed852f6ceb5790ff15e5d2232c8b42e3ae1
forafish <fora.luo@gmail.com>
2017-05-08 16:37:33 +0800

    Add files

---
+++ packages/node_modules/feathers-service/src/hooks.js
@@ -0,0 +1,19 @@
+export const beforeHook = {
+  all: [],
+  find: [],
+  get: [],
+  create: [],
+  update: [],
+  patch: [],
+  remove: []
+};
+
+export const afterHook = {
+  all: [],
+  find: [],
+  get: [],
+  create: [],
+  update: [],
+  patch: [],
+  remove: []
+};
