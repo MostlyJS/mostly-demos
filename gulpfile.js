75b4b6d0a27af1ab1778e1ae381db29206fe193e
forafish <fora.luo@gmail.com>
2017-05-10 20:07:20 +0800

    Fix package deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 const gulp = require('gulp');
-const gutil = require('gulp-util');
 const spawn = require('child_process').spawn;
 const glob = require('glob');
 const path = require('path');
-const util = require('util');
 
 const gulpfiles = './packages/node_modules/*/gulpfile.js';
 
