a37ae0c1f6931961d57dab06b0e8b98d822b0443
forafish <fora.luo@gmail.com>
2017-05-10 19:29:33 +0800

    Separate gulpfile for each package

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,21 @@
-const fs = require('fs');
-const path = require('path');
 const gulp = require('gulp');
-const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
-const pm2 = require('pm2');
-const nodemon = require('gulp-nodemon');
-const rimraf = require('rimraf');
-const eslint = require('gulp-eslint');
-const changed = require('gulp-changed');
 const gutil = require('gulp-util');
-const MonorepoTasks = require('gulp-tasks-monorepo');
+const spawn = require('child_process').spawn;
+const glob = require('glob');
+const path = require('path');
+const util = require('util');
 
-var repo = MonorepoTasks({
-  dir: path.join(__dirname, '/packages/node_modules')
-});
+const gulpfiles = './packages/node_modules/*/gulpfile.js';
 
-repo.task('lint', function(pkg) {
-  gutil.log('Lint', pkg.name(), 'package');
-  return gulp.src(path.join(pkg.location(), 'src/**/*.js'))
-    .pipe(changed('lib'))
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
-});
-
-repo.task('clean', function clean(pkg, done) {
-  gutil.log('Clean', pkg.name(), 'package');
-  rimraf(path.join(pkg.location(), '/lib'), done);
-});
-
-repo.task('compile', (pkg) => {
-  gutil.log('Compile', pkg.name(), 'package');
-  return gulp.src(path.join(pkg.location(), 'src/**/*.js'))
-    .pipe(changed('lib'))
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(path.join(pkg.location(), 'lib')));
-});
-
-repo.task('server', function (pkg) {
-  gutil.log('Server', pkg.name(), 'package');
-  var configFile = path.join(pkg.location(), 'process_development.json');
-  fs.readFile(configFile, function(err, data) {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        gutil.log(' => Skip not a server', pkg.name());
-        return;
-      }
-      throw err;
-    }
-    var config = JSON.parse(data);
-    config.apps.forEach(function(app) {
-      app.script = path.join(pkg.location(), app.script);
-      app.error_file = path.join(pkg.location(), app.error_file);
-      app.out_file = path.join(pkg.location(), app.out_file);
-    });
-    pm2.connect(true, function (err) {
-      if (err) throw err;
-      pm2.restart(config, function (err, apps) {
-        if (err) throw err;
-        gutil.log('Server started', pkg.name(), 'package');
-        pm2.streamLogs('all', 0);
-      });
-    });
+// Task for each package
+var files = glob.sync(path.join(__dirname, gulpfiles));
+var tasks = [];
+files.forEach(function(file) {
+  var package = path.basename(path.dirname(file));
+  gulp.task(package, function() {
+    var child = spawn('gulp', ['--gulpfile', file], { stdio: 'inherit' });
   });
+  tasks.push(package);
 });
 
-repo.task('watch', function(pkg) {
-  gutil.log('Watch', pkg.name(), 'package');
-  gulp.watch(path.join(pkg.location(), 'src/**/*.js'), ['compile', 'server'])
-});
-
-repo.task('default', ['compile', 'server', 'watch']);
+gulp.task('default', tasks);
