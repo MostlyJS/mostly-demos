0a0442ea59d21c03760cd42e24b95c45f5c828a3
forafish <fora.luo@gmail.com>
2018-04-17 16:49:40 +0800

    update run_development

--- bin/run_development.sh
+++ bin/run_development.sh
@@ -4,8 +4,11 @@ for pkg in $(ls packages/node_modules); do
   if [ ! -d "packages/node_modules/$pkg" ]; then
     continue
   fi
+  if [ ! -e "packages/node_modules/$pkg/process_development.json" ]; then
+    continue
+  fi
   cd packages/node_modules/$pkg
-  echo "Yarn install node_modules $pkg..."
+  echo "Pm2 start node_modules $pkg..."
   pm2 start process_development.json
   cd -
 done
